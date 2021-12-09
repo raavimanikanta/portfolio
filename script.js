1b40baa6dc4b39ad1e97d47aa40342e43923b4c6
raavimanikanta <raavi,ani123@gmail.com>
2021-12-09 14:08:08 +0530

    second changes

---
+++ script.js
@@ -0,0 +1,7 @@
+const navbar=document.querySelector( ".navbar");
+const mobile_menu=document.querySelector("ul");
+const header=document.querySelector(".header .container");
+navbar .addEventListener("click"()=>{
+mobile_menu .classlist.toggle("active");
+}
+)
